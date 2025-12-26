2f6a01105498094deb7bf3a33e04ee5224e7e1de
wijdan-hub <mwijdan62@gmail.com>
2025-12-26 16:28:24 +0700

    Initial commit

---
+++ src/test/java/com/wijdan/testing/automation/base/BaseTest.java
@@ -0,0 +1,25 @@
+package com.wijdan.testing.automation.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.demoblaze.com");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
